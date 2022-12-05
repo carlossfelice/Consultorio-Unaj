c3eb33881767ad0fbd6c1248ebc32f4a49b350d9
carlitos <44775871+carlossfelice@users.noreply.github.com>
2022-12-05 20:36:50 -0300

    Add files via upload

---
+++ TpFinal_V8/TpFinal_V7/TpFinal/Paciente.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 16/10/2022
+ * Time: 16:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TpFinal
+{
+	/// <summary>
+	/// Description of Paciente.
+	/// </summary>
+	public class Paciente
+	{
+		//**y el paciente tiene nombre, dni,obra social, nro afiliado y diagnóstico
+		//atributos
+		private int nroafiliado,dni;
+		private	string nombre,obrasocial,diagnostico;
+		
+		//constructores
+		
+		public Paciente(){}
+		public Paciente(string nombre,int dni,string obrasocial,
+		                int nroafiliado,string diagnostico){
+			this.nombre=nombre;
+			this.dni=dni;
+			this.obrasocial=obrasocial;
+			this.nroafiliado=nroafiliado;
+			this.diagnostico=diagnostico;
+		}
+		
+		//propiedades
+		public string Nombre	{set{nombre=value;}get{return nombre;}}
+		public int 		DNI		{set{dni=value;}get{return dni;}}
+		public string ObraSocial{set{obrasocial=value;}get{return obrasocial;}}
+		public int 	 NroAfiliado{set{nroafiliado=value;}get{return nroafiliado;}}
+		public string Diagnostico{set{diagnostico=value;}get{return diagnostico;}}
+		//metodos
+		public void ImprimirPaciente(){
+			Console.WriteLine(
+				"Nombre:  "+nombre+"\n"+
+				"DNI:  "+dni+"\n"+
+				"ObraSocial:  "+obrasocial+"\n"+
+				"NroAfiliado:  "+nroafiliado+"\n"+
+				"Diagnostico:  "+diagnostico+"\n"
+			);
+		}
+	}
+}
---
+++ TpFinal_V8/TpFinal_V7/TpFinal/Turno.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Carlos
+ * Date: 15/10/2022
+ * Time: 16:35
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace TpFinal
+{
+	/// <summary>
+	/// Description of Turno.
+	/// </summary>
+	public class Turno
+	{
+		private string horaTurno,nombrePaciente,estado;
+		//atributos
+		//el turno tiene un nombre de paciente y la hora del turno
+		
+		//constructores
+		public Turno(string horaTurno,string nombrePaciente,string estado){
+			this.nombrePaciente=nombrePaciente;
+			this.horaTurno=horaTurno;
+			this.estado=estado;
+		}
+		
+		//propiedades
+		public string HoraTurno{
+			set{horaTurno=value;}get{return horaTurno;}
+		}
+		public string NombrePTurno{
+			set{nombrePaciente=value;}get{return nombrePaciente;}
+		}
+		public string EstadoTurno{
+			set{estado=value;}get{return estado;}
+		}
+		//metodos
+		
+		public void ImprimirTurno(){
+			Console.WriteLine
+				(
+					"HoraTurno: "+horaTurno+"\n"+
+					"NombrePaciente: "+nombrePaciente+"\n"+
+					"Estado: "+estado+"\n"
+				);
+		}
+		
+		
+	}
+}
